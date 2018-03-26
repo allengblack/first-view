6f3ee91fa91ac675f241e44db2633b81c4b58c39
Gbolahan Allen <allenforjob@gmail.com>
2018-03-26 16:58:22 +0100

    initial

---
+++ App.vue
@@ -0,0 +1,39 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello, Vue!';
+    }
+});
+
+var app = new Vue({
+            el: '#app',
+            data: {
+                message: 'Hello, Vue!'
+            }
+        });
+
+        var app2 = new Vue({
+            el: '#app-2',
+            data: {
+                dynamic: 'I hovered at: ' + new Date().toLocaleString()
+            }
+        });
+
+        var clickSide = new Vue({
+            el: '#click-side',
+            data: {
+                value: false,
+                cows: [
+                    "cow1",
+                    "cow2",
+                    "cow3"
+                ]
+            },
+            methods: {
+                changeValue: function () {
+                    this.value = !this.value;
+                }
+            }
+        });
+
+    
